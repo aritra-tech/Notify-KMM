4662108013a23950a8206d4b7d930dda2a0cc267
aritra <aritrarick2002@gmail.com>
2023-10-31 17:39:55 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NotifyKMM"
+include(":androidApp")
+include(":shared")
