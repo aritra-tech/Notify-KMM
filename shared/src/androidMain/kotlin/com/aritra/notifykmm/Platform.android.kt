af2c8b1b9e433ab6044e022b4cd3b3b1dd205b2b
aritra <aritrarick2002@gmail.com>
2023-10-31 20:33:07 +0530

    🧑‍🔧 Fixed the code structure

--- shared/src/androidMain/kotlin/com/aritra/notifykmm/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.aritra.notifykmm
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
