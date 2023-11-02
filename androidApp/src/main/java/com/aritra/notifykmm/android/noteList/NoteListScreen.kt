d447c9a63100bb3808a1a53119336a06f40f1250
aritra <aritrarick2002@gmail.com>
2023-11-02 19:34:02 +0530

    ♻️ `NoteList` screen created

---
+++ androidApp/src/main/java/com/aritra/notifykmm/android/noteList/NoteListScreen.kt
@@ -0,0 +1,82 @@
+package com.aritra.notifykmm.android.noteList
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteListScreen(
+    viewModel: NoteListViewModel = hiltViewModel()
+) {
+
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.loadNotes()
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+            }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                HideableSearchTextField(
+                    text = state.searchText,
+                    isSearchActive = state.isSearchActive,
+                    onTextChange = viewModel::onSearchTextChange,
+                    onSearchClick = viewModel::onToggleSearch,
+                    onCloseClick = viewModel::onToggleSearch,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                )
+            }
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+            ){
+                items(state.notes) {note ->
+                    NoteItem(
+                        note = note,
+                        onNoteClick = { /*TODO*/ },
+                        modifier = Modifier.fillMaxWidth().padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
